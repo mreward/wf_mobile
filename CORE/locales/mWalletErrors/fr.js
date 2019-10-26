b4133d673ed904d6e0da6606d159d65b6739a3f5
Igor Sokolenko <sokolenkoigor32@gmail.com>
2019-10-26 22:35:55 +0300

    autogenerated locales

---
+++ CORE/locales/mWalletErrors/fr.js
@@ -0,0 +1,6 @@
+export default {
+    m_mwallet_400: 'Une erreur est survenue sur le serveur. Réessayez plus tard.',
+    m_mwallet_500: 'Une erreur est survenue sur le serveur. Réessayez plus tard.',
+    m_mwallet_704: 'Erreur de code SMS',
+    m_mwallet_707: 'Mauvais code SMS'
+}
