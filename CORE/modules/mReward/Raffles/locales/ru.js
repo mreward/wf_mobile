5a7b920b749a327758c4ef9ec85b104ee2b81849
Igor Sokolenko <sokolenkoigor32@gmail.com>
2019-10-29 22:16:15 +0200

    Raffles: page-raffle-details fornextdib text locales

--- CORE/modules/mReward/Raffles/locales/ru.js
+++ CORE/modules/mReward/Raffles/locales/ru.js
@@ -5,5 +5,5 @@ export default {
     m_raffles_for_next_dib_start: 'Осталось',
     m_raffles_my_dibs: 'Мои фишки',
     m_raffles_period_dibs_accumulation: 'Период накопления фишек',
-    m_raffles_residue_from_last_purchase: 'Остаток с предидущей покупки'
+    m_raffles_residue_from_last_purchase: 'Остаток с предыдущей покупки'
 }
